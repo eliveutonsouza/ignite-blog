a86aab497bc276096d25033a7b9020e2217d79da
Eliveuton Souza <eliveuton3m@hotmail.com>
2023-12-30 14:10:10 -0300

    Finish Project

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import "./global.css";
+import styles from "./App.module.css";
+
+import { Header } from "./components/Header.jsx";
+import { Sidebar } from "./components/Sidebar.jsx";
+import { Post, PostType } from "./components/Post.jsx";
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/eliveutonsouza.png",
+      name: "Eliveuton Souza",
+      role: "Desenvolvedor Fron-End",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-12-09 20:00:00"),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-12-12 20:00:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
